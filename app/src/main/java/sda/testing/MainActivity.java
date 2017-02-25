0694202a0a094937ff28b0db8688be2ae7b133f4
kawa89 <michal.kawalko1@gmail.com>
2017-02-25 16:24:48 +0100

    Make exception

--- app/src/main/java/sda/testing/MainActivity.java
+++ app/src/main/java/sda/testing/MainActivity.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ArrayList<Integer> list;
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        // make the exception here
+        list.add(1);
+        text.setText(Integer.toString(list.size()));
     }
 }
