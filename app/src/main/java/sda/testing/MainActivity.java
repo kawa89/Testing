c8cdaf3e25e14b356b12c8bf06905300c4515b69
kawa89 <michal.kawalko1@gmail.com>
2017-02-25 18:23:36 +0100

    Make exception

--- app/src/main/java/sda/testing/MainActivity.java
+++ app/src/main/java/sda/testing/MainActivity.java
@@ -4,8 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +25,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+        text = (TextView) findViewById(R.id.text);
     }
 
     @Override
     public void onClick(View view) {
-        // make the exception here
+
+        URL url = null;
+        try {
+            url = new URL("http://www.android.com/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream it = new BufferedInputStream(urlConnection.getInputStream());
+            InputStreamReader read = new InputStreamReader(it);
+            BufferedReader buff = new BufferedReader(read);
+            final StringBuilder dta = new StringBuilder();
+            String chunks;
+            while ((chunks = buff.readLine()) != null) {
+                dta.append(chunks);
+            }
+            text.setText(dta);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
