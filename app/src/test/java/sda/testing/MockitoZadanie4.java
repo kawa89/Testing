da81800dabeafa7e3c99270c028e1d3cb27ee9ad
kawa89 <michal.kawalko1@gmail.com>
2017-03-05 19:22:44 +0100

    mockito_zadania

---
+++ app/src/test/java/sda/testing/MockitoZadanie4.java
@@ -0,0 +1,10 @@
+package sda.testing;
+
+public class MockitoZadanie4 {
+
+    // 1. Make a MainActivity mock, that has mocked Resources, when anyInt is passed to getString() always a "NotTesting" String is returned
+    // 2. Make a MainActivity mock, that has mocked Resources, when only R.string.app_name is passed to getString(), always a "NotTesting" String
+    //    is returned. Check if other string is not overrided.
+
+
+}
